2710a3452989f0fbbe84491496b18c0a14116c07
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2023-02-04 22:24:05 -0500

    Week1

---
+++ Week1/kata.py
@@ -0,0 +1,22 @@
+i = 1
+if i < 2:
+  print(i)
+  i += 1
+while i < 3:
+    print(i)
+    i += 1
+while i < 5:
+    print("Pepsi")
+    i += 1
+while i < 6:
+    print("Coke")
+    i += 1
+while i < 10:
+    print("Pepsi")
+    i += 1
+while i < 15:
+    print("Coke")
+    i += 1
+while i < 16:
+    print("PepsiCoke")
+    i += 1   
