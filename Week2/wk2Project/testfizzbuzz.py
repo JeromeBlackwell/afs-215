c0c8b4c00751efbab6d2c131073f25ee08bf3d0b
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2023-02-08 23:58:26 -0500

    Week2

--- Week2/wk2Project/testfizzbuzz.py
+++ Week2/wk2Project/testfizzbuzz.py
@@ -1,7 +1,17 @@
+def PepsiCoke(num):
+    if num < 3:
+        print(str(num))
+    elif num % 3 == 0 and num % 5 == 0:
+        print("PepsiCoke")
+    elif num % 3 == 0:
+        print("Pepsi")
+    elif num % 5 == 0:
+        print("Coke")
 
-def func(x):
-    return x + 1
-
-
-def answer():
-    assert func(3) == 5
+PepsiCoke(1)
+PepsiCoke(2)
+PepsiCoke(3)
+PepsiCoke(5)
+PepsiCoke(6)
+PepsiCoke(10)
+PepsiCoke(15)
