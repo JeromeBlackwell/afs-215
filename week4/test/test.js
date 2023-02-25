e34eb38bc083f20aaff8379fad95ffa222179853
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2023-02-24 21:44:38 -0500

    week4

---
+++ week4/test/test.js
@@ -0,0 +1,11 @@
+const {expect} = require('chai');
+
+describe('My Calculator', () => {
+    it('expect passing', () => {
+        expect(true).to.equal(true) // expected true to equal false
+       
+    })
+})
+// it('expect passing', function(){
+//     expect(true).to.equal() // expected true to equal false   
+// }))
