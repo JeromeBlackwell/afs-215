46f7009453dae91ae516e2ab64196c22a8b248fe
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2023-02-27 02:00:25 -0500

    week4

--- week4/calculator.js
+++ week4/calculator.js
@@ -1,5 +1,20 @@
 module.exports = class Calculator {
-    constructor(num) {
-     this.num = num;
+    constructor(num1, num2) {
+     this.num1 = num1;
+     this.num2 = num2;
     }
-    Greeting(){
+    add() {
+        return this.num1 + this.num2 
+        }
+    sub() {
+        return this.num1 + this.num2 
+        }
+    mult() {
+        return this.num1 + this.num2 
+        }
+    div() {
+        return this.num1 + this.num2 
+        }
+    }
+    
+    
--- week4/test/testcalculator.js
+++ week4/test/testcalculator.js
@@ -1,1 +1,9 @@
-var Instances = require('./conditional.js')
+var Calculator = require('./calculator.js')
+
+var user1 = new Calculator(4,11)
+
+console.log(user1.add())
+console.log(user1.sub())
+console.log(user1.mult())
+console.log(user1.div())
+
