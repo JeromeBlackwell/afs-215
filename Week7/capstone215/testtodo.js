c8e7650db70e2940627f34a7aad42d1676cb758a
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2023-03-13 17:46:26 -0400

    week7

--- Week7/capstone215/testtodo.js
+++ Week7/capstone215/testtodo.js
@@ -14,6 +14,7 @@ describe('My Today List', () => {
         
     })
     //#2 add item
+    // # original code
     it('AddTo List', () => {
         expect(Instance.addTo('Pack Plane Ticket')).to.deep.equal(['Pack Plane Ticket']) // expected true to equal false    
     })
@@ -24,26 +25,36 @@ describe('My Today List', () => {
     })
     //#3
     // Add Multi Items
+    // original code
+    // it('Multi ToDo List', () => {
+    //     expect(Instance.addMultTodo('Contact Hotel',25,['Schedule pick-up at 4:45am'],{A:1, B:2}, true)).to.deep.equal(['Contact Hotel',25,['Schedule pick-up at 4:45am'],{A:1, B:3},true]) // expected true to equal false
+    // })
+
+    // refactored code    
     it('Multi ToDo List', () => {
         expect(Instance.addMultTodo('Contact Hotel',25,['Schedule pick-up at 4:45am'],{A:1, B:2}, true)).to.deep.equal(['Contact Hotel',25,['Schedule pick-up at 4:45am'],{A:1, B:2},true]) // expected true to equal false
              
     })
     //#4 remove first item
+    // #original code
     it('RemoveFirstItem', () => {
         expect(Instance.removeFirstItem()) // expected true to equal false
 
     })
     //#5 remove last item
+    // #original code
     it('RemoveLastItem', () => {
         expect(Instance.removeLastItem()) // expected true to equal false
 
     })
     //#6 remove specific item
+    // #original code
     it('RemoveSpecItem', () => {
         expect(Instance.removeSpecItem('Pickup Tuxedo')) // expected true to equal false
 
     })    
-    //# refactor code, implement beforeEach, before, after and afterEach
+    //# original code, implement beforeEach, before, after and afterEach
+    // #original code
     beforeEach(function(){
         Instance = new Exotics()
     })
